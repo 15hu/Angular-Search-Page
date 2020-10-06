a2e7f76d1d16d1c8174a9e9f36595a930d403764
15hu <vishu.damahe1999@gmail.com>
2020-10-06 11:57:51 +0530

    added autocomplete search field for datasets

--- src/searchApp.controller.js
+++ src/searchApp.controller.js
@@ -8,15 +8,50 @@ function SearchController(){
     var searchCtrl = this;
 
     //dataset names or indexes names in elasticsearch
-    searchCtrl.datasets = ["Dataset 1","Dataset 2","Dataset 3","Dataset 4","Dataset 5","Dataset 6","Dataset 7",
-    "Dataset 8","Dataset 9","Dataset 10","Dataset 11","Dataset 12","Dataset 13","Dataset 14","Dataset 15","Dataset 16"];
+searchCtrl.datasets = [
+                {name: 'dataset 1', code: '1'},
+                {name: 'dataset 2', code: '2'},
+                {name: 'dataset 3', code: '3'},
+                {name: 'dataset 4', code: '4'},
+                {name: 'dataset 5', code: '5'},
+                {name: 'dataset 6', code: '6'},
+                {name: 'dataset 7', code: '7'},
+                {name: 'dataset 8', code: '8'},
+                {name: 'dataset 9', code: '9'},
+                {name: 'dataset 10', code: '10'},
+                {name: 'dataset 11', code: '11'},
+                {name: 'dataset 12', code: '12'},
+                {name: 'dataset 13', code: '13'},
+                {name: 'dataset 14', code: '14'},
+                {name: 'dataset 15', code: '15'}];
 
     //dataset selected by user or index in which search is to be performed
-    searchCtrl.selectedDataset = undefined;
+    searchCtrl.selectedDataset = undefined;   //equal to searchCtrl.selectedDatasetObject.originalObject.name
+    searchCtrl.selectedDatasetObject = null;  //returned by angucomplete directory
+
     //Keyword entered by the user to search in the index 'searchCtrl.selectedDataset'
     searchCtrl.keywordToSearch = undefined;
 
-    searchCtrl.selectedTagObject = null;
+    searchCtrl.tags = [
+                {name: 'tag1', code: '1'},
+                {name: 'tag2', code: '2'},
+                {name: 'tag3', code: '3'},
+                {name: 'tag4', code: '4'},
+                {name: 'tag5', code: '5'},
+                {name: 'tag6', code: '6'},
+                {name: 'tag7', code: '7'},
+                {name: 'tag8', code: '8'},
+                {name: 'tag9', code: '9'},
+                {name: 'tag10', code: '10'},
+                {name: 'tag11', code: '11'},
+                {name: 'tag12', code: '12'},
+                {name: 'tag13', code: '13'},
+                {name: 'tag14', code: '14'},
+                {name: 'tag15', code: '15'}];
+
+    //tag selected by the user
+    searchCtrl.selectedTag = undefined;     //equal to searchCtrl.selectedTagObject.originalObject.name
+    searchCtrl.selectedTagObject = null;    //returned by angucomplete directory
 
     searchCtrl.checkButton = function(){
         if(searchCtrl.selectedDataset === undefined || searchCtrl.keywordToSearch === undefined || searchCtrl.selectedTagObject === null){
@@ -109,8 +144,10 @@ function SearchController(){
                 _source : "file 5 content"
             }];
 
+    searchCtrl.viewSearchFileName = "None";
     searchCtrl.viewSearchFile = "Please Select a file.";
     searchCtrl.viewThisSearchFile = function(index){
+        searchCtrl.viewSearchFileName = searchCtrl.searchResults[index]._id;
         searchCtrl.viewSearchFile = searchCtrl.searchResults[index]._source;
     }
     searchCtrl.viewFileName = "None."
@@ -119,22 +156,9 @@ function SearchController(){
         searchCtrl.viewFileName = searchCtrl.allFiles[index]._id;
         searchCtrl.viewFile = searchCtrl.allFiles[index]._source;
     }
-    searchCtrl.selectedFileObject = null;
-    searchCtrl.tags = [
-                {name: 'tag1', code: '1'},
-                {name: 'tag2', code: '2'},
-                {name: 'tag3', code: '3'},
-                {name: 'tag4', code: '4'},
-                {name: 'tag5', code: '5'},
-                {name: 'tag6', code: '6'},
-                {name: 'tag7', code: '7'},
-                {name: 'tag8', code: '8'},
-                {name: 'tag9', code: '9'},
-                {name: 'tag10', code: '10'},
-                {name: 'tag11', code: '11'},
-                {name: 'tag12', code: '12'},
-                {name: 'tag13', code: '13'},
-                {name: 'tag14', code: '14'},
-                {name: 'tag15', code: '15'}];
+
+    searchCtrl.selectedFile = undefined;      //equal to searchCtrl.selectedFileObject.originalObject.name
+    searchCtrl.selectedFileObject = null;     //returned by angucomplete directory
+
 }
 })();
